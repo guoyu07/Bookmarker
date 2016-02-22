5ec2a4b62af5169285b9d46ae298f477b70675fb
WenTao <tonnie.lwt@gmail.com>
2016-02-22 23:57:01 +0800

    fix bug

--- bookmarker/bookmarker/settings/base.py
+++ bookmarker/bookmarker/settings/base.py
@@ -127,13 +127,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
-STATICFILES_DIRS = [
-    os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static'),
+# ]
 
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 MEDIA_URL = '/media/'
 
